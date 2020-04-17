11a396a307f54c490e6bc6f870ba33978822ed71
mikosramek <miko@mikosramek.ca>
2020-04-17 12:23:50 -0400

    Updates

---
+++ precache-manifest.bf0544e9d55242fa6159d9a67068680a.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcb20455d3517bafe8f19d390288b795",
+    "url": "/CompStudio2020/index.html"
+  },
+  {
+    "revision": "3dc592a6496d9815c90d",
+    "url": "/CompStudio2020/static/css/main.07e70334.chunk.css"
+  },
+  {
+    "revision": "ee8d1d62ba4573e10796",
+    "url": "/CompStudio2020/static/js/2.4037d57c.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/CompStudio2020/static/js/2.4037d57c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dc592a6496d9815c90d",
+    "url": "/CompStudio2020/static/js/main.1302eb70.chunk.js"
+  },
+  {
+    "revision": "eac976a71a020351bf4a",
+    "url": "/CompStudio2020/static/js/runtime-main.92ae82ac.js"
+  },
+  {
+    "revision": "c2ae738aef2e04008524a80f37eff45f",
+    "url": "/CompStudio2020/static/media/RF-1.c2ae738a.svg"
+  },
+  {
+    "revision": "60f40b9cd202f98a8d418b5e5234d12d",
+    "url": "/CompStudio2020/static/media/TF-1.60f40b9c.svg"
+  },
+  {
+    "revision": "da70a333544b9d25deb63b3e5532d24a",
+    "url": "/CompStudio2020/static/media/TF-2.da70a333.svg"
+  },
+  {
+    "revision": "ef584f79115abff5d622f5e9eb12c6e2",
+    "url": "/CompStudio2020/static/media/arrow.ef584f79.svg"
+  },
+  {
+    "revision": "522921b85ec2f93e4e39af4580eada49",
+    "url": "/CompStudio2020/static/media/blob.522921b8.svg"
+  },
+  {
+    "revision": "c3a80139f2c03d9b113771fc5752664c",
+    "url": "/CompStudio2020/static/media/blob_2.c3a80139.svg"
+  },
+  {
+    "revision": "7053df8ba3246ecc2a15bf54d75a2cce",
+    "url": "/CompStudio2020/static/media/bubble.7053df8b.svg"
+  },
+  {
+    "revision": "98dabf73279e22de15be43e2e693ce7d",
+    "url": "/CompStudio2020/static/media/girl.98dabf73.svg"
+  },
+  {
+    "revision": "3829639ddd2474c58ed76e94f8929ca8",
+    "url": "/CompStudio2020/static/media/girl_sad.3829639d.svg"
+  },
+  {
+    "revision": "75e070777ff312321e0bea3b1504a68f",
+    "url": "/CompStudio2020/static/media/intro-1.75e07077.svg"
+  },
+  {
+    "revision": "9f8e0c003b6a470038f54f04181209f1",
+    "url": "/CompStudio2020/static/media/intro-2.9f8e0c00.svg"
+  },
+  {
+    "revision": "1567b09f6f262eea3af4cbff3b50bd5c",
+    "url": "/CompStudio2020/static/media/meteor.1567b09f.svg"
+  },
+  {
+    "revision": "ce0b1890166b0d4cedf76f347efeda4f",
+    "url": "/CompStudio2020/static/media/sadface.ce0b1890.svg"
+  },
+  {
+    "revision": "be0fc92002f9a2d0982374a69001a7c3",
+    "url": "/CompStudio2020/static/media/sky.be0fc920.svg"
+  },
+  {
+    "revision": "15d619bb9cb764aba6bd617f5204e312",
+    "url": "/CompStudio2020/static/media/sticky.15d619bb.svg"
+  },
+  {
+    "revision": "2e5524e6e2183cfc06950f65d9cbeda3",
+    "url": "/CompStudio2020/static/media/ticketbg.2e5524e6.svg"
+  }
+]);
